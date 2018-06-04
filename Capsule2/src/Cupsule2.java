4eb6bac0d3ef42c9da4aed2218803ce6b97e3a8e
mountain-book <tetsuronron@gmail.com>
2018-06-04 10:46:38 +0900

    0604

--- Capsule2/src/Cupsule2.java
+++
@@ -1,9 +0,0 @@
-
-public class Cupsule2 {
-
-	public static void main(String[] args) {
-		Person2 taro = new Person2("山田太郎",20);
-		System.out.println(taro.getName());
-	}
-
-}
--- Iphone3/src/Mp3Player.java
+++
@@ -1,7 +0,0 @@
-
-public interface Mp3Player {
-	public abstract void play();
-	public void stop();
-	public void next();
-	public void back();
-}
